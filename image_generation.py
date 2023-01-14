db84193eabc1ebde77436bfaddac2d014e3fecd8
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-01-14 15:24:04 +0530

    updated image generation

---
+++ image_generation.py
@@ -0,0 +1,37 @@
+"""
+A Simple Python Instruction Guide on how to use OpenAI API for Image Generation
+"""
+
+import openai, sys
+
+class ImageGenerator(type):
+    
+    __slots__ = [
+        'IMAGE_HEIGHT',
+        'IMAGE_WIDTH',
+        'NO_OF_IMAGES_TO_GENERATE'
+    ]
+    
+    def __init__(self):
+        self.IMAGE_HEIGHT               = 1024
+        self.IMAGE_WIDTH                = 1024
+        self.NO_OF_IMAGES_TO_GENERATE   = 1
+        openai.api_key                  = "" ## Your API key - Look at Instructions in README.md
+
+    def generate_image(self, my_promt):
+        try:
+            response = openai.Image.create(
+                prompt              = my_promt,
+                n                   = self.NO_OF_IMAGES_TO_GENERATE,
+                size                = f"{self.IMAGE_HEIGHT}x{self.IMAGE_WIDTH}"
+            )
+            image_url = response['data'][0]['url']
+            print(f"[URL] Please go to the following URL to see your Generated Image: {image_url}")
+        except Exception as e:
+            sys.exit(f"Oops! Something went wrong while generating your image. Error - {e}")
+
+
+if __name__ == '__main__':
+    ig = ImageGenerator()
+    ig.generate_image(input("Describe your Image that you wish to be generated: "))
+
